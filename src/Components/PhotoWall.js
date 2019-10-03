174ccf63de071d68516cddb6103edd1c1ec3c3ae
Patrik <szabo.patrik@e-vista.hu>
2019-10-03 13:37:30 +0200

    features

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Photo from './Photo';
+import PropTypes from 'prop-types';
+
+function PhotoWall(props) {
+  return (
+    <div className="photoGrid">
+      {props.posts.map((post, index) => (
+        <Photo
+          key={index}
+          post={post}
+          post={post}
+          onRemovePhoto={props.onRemovePhoto}
+        />
+      ))}
+    </div>
+  );
+}
+
+PhotoWall.propTypes = {
+  posts: PropTypes.array.isRequired,
+  onRemovePhoto: PropTypes.func.isRequired
+};
+
+export default PhotoWall;
