0f2123326b6bf64e765b3ff4ef5381a088cab0f6
Patrik <szabo.patrik@e-vista.hu>
2019-10-08 10:33:45 +0200

    new functions

--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -16,14 +16,17 @@ function PhotoWall(props) {
         +{' '}
       </button> */}
       <div className="photoGrid">
-        {props.posts.map((post, index) => (
-          <Photo
-            key={index}
-            post={post}
-            post={post}
-            onRemovePhoto={props.onRemovePhoto}
-          />
-        ))}
+        {props.posts
+          .sort(function(x, y) {
+            return y.id - x.id;
+          })
+          .map((post, index) => (
+            <Photo
+              key={index}
+              post={post}
+              onRemovePhoto={props.onRemovePhoto}
+            />
+          ))}
       </div>
     </div>
   );
